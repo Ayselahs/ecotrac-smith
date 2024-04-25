7744316fb2e024db5c605f85faffc6497ce8b580
Ayselah Smith <ayselahs@gmail.com>
2024-04-24 22:22:25 -0500

    auth fix

--- pages/api/auth/[action].js
+++ pages/api/auth/[action].js
@@ -6,7 +6,7 @@ export default withIronSessionApiRoute(
   function handler(req, res) {
     if (req.method !== 'POST')
       return res.status(404).end()
-    switch(req.query.action) {
+    switch (req.query.action) {
       case "login":
         return login(req, res)
       case "logout":
@@ -30,8 +30,8 @@ async function login(req, res) {
     req.session.user = otherFields
     await req.session.save()
     res.status(200).end()
-  } catch(err) {
-    res.status(400).json({error: err.message})
+  } catch (err) {
+    res.status(400).json({ error: err.message })
   }
 }
 async function logout(req, res) {
@@ -40,15 +40,15 @@ async function logout(req, res) {
 }
 async function signup(req, res) {
   try {
-    const {username, password} = req.body
+    const { username, password } = req.body
     const {
       password: _,
       ...otherFields
     } = await db.user.create(username, password)
     req.session.user = otherFields
     await req.session.save()
-    res.redirect('/dashboard')
-  } catch(err) {
-    res.status(400).json({error: err.message})
+    res.redirect('/loggedDashboard')
+  } catch (err) {
+    res.status(400).json({ error: err.message })
   }
 }
